260f899428d7ba9657c6f1cce06d2d4efee8ad14
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-13 18:49:28 -0700

    commit

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  const data = JSON.parse(body);
+  const breed = data[0];
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, resp, body) => {
+  
+    if (error) {
+      callback(`Failed to request details: ${error}`,null);
+    }
+
+    if (breed) {
+      callback(null,breed.description);
+    } else {
+      callback(`Unable to find ${breedName}`,null);
+    }
+  });
+
+};
