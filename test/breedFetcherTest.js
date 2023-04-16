20e82ad7c3b31279e6498fcc7b45713a7a11ecc3
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-15 21:01:07 -0700

    Update breedFetcherTest.js

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -4,19 +4,19 @@ const { assert } = require('chai');
 describe('fetchBreedDescription', () => {
   it('returns a string description for a valid breed, via callback', (done) => {
     fetchBreedDescription('Siberian', (err, desc) => {
-      // we expect no error for this scenario
+    
       assert.equal(err, null);
 
       const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
 
-      // compare returned description
+ 
       assert.equal(expectedDesc, desc.trim());
 
       done();
     });
 
     it('returns error when breed is not known.', (done)) => {
-      feedBreedDescription('GoldenRetriever,', err, desc) => {
+      feedBreedDescription('GoldenRetriever,', err, desc) => { //golden does not exist in cat world
       assert.exists(err);
       assert.equal(undefined, desc);
       done();
